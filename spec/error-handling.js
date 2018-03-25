0abda11f208a684bb2d2cb3820345ead468d18c6
jameswilddev <jameswilddev@outlook.com>
2018-03-25 20:08:26 +0100

    Errors now stop the render loop.

--- spec/error-handling.js
+++ spec/error-handling.js
@@ -6,10 +6,12 @@ describe("errorHandling", () => {
   it("defines errorEncountered as false", () => expect(index.__get__("errorEncountered")).toBeFalsy())
 
   describe("handleError", () => {
-    let setStatus
+    let setStatus, stopRenderLoop
     beforeEach(() => {
       setStatus = jasmine.createSpy("setStatus")
       index.__set__("setStatus", setStatus)
+      stopRenderLoop = jasmine.createSpy("stopRenderLoop")
+      index.__set__("stopRenderLoop", stopRenderLoop)
       index.__get__("handleError")("Test Message", "Test Source File", 398, 289, "Test Error")
     })
     it("sets the status once", () => expect(setStatus).toHaveBeenCalledTimes(1))
@@ -17,6 +19,7 @@ describe("errorHandling", () => {
       `Unhandled error on line 398, column 289 of "Test Source File":
 \t"Test Message"
 \t"Test Error"`))
+    it("stops the render loop once", () => expect(stopRenderLoop).toHaveBeenCalledTimes(1))
     it("sets errorEncountered to true", () => expect(index.__get__("errorEncountered")).toBeTruthy())
   })
 })
--- src/error-handling.js
+++ src/error-handling.js
@@ -3,4 +3,5 @@ let errorEncountered = false
 function handleError(message, source, lineno, colno, error) {
   errorEncountered = true
   setStatus("Unhandled error on line " + lineno + ", column " + colno + " of \"" + source + "\":\n\t\"" + message + "\"\n\t\"" + error + "\"")
+  stopRenderLoop()
 }
