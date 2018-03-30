b48bac1f2417e55b5987e8497665dd9781091e77
jameswilddev <jameswilddev@outlook.com>
2018-03-30 21:49:26 +0100

    Implemented file parsing helpers.

--- spec/file.js
+++ spec/file.js
@@ -83,4 +83,155 @@ describe("file", () => {
       it("does not log an error", () => expect(console.error).not.toHaveBeenCalled())
     })
   })
+
+  describe("createParser", () => {
+    let arrayBuffer, parser
+    beforeEach(() => {
+      arrayBuffer = new Uint8Array([77, 27, 240, 195, 116]).buffer
+      parser = index.__get__("createParser")(arrayBuffer)
+    })
+    it("does not modify the array buffer", () => expect(Array.from(new Uint8Array(arrayBuffer))).toEqual([77, 27, 240, 195, 116]))
+    it("returns a data view", () => expect(parser.dataView).toEqual(jasmine.any(DataView)))
+    it("returns a data view into the array buffer", () => expect(parser.dataView.buffer).toBe(arrayBuffer))
+    it("returns a data view from the start of the array buffer", () => expect(parser.dataView.byteOffset).toEqual(0))
+    it("returns a data view to the end of the array buffer", () => expect(parser.dataView.byteLength).toEqual(5))
+    it("returns a position of zero", () => expect(parser.position).toEqual(0))
+  })
+
+  describe("parseUint8", () => {
+    let arrayBuffer, dataView, parser, result
+    beforeEach(() => {
+      arrayBuffer = new Uint8Array([77, 27, 240, 195, 116]).buffer
+      dataView = new DataView(arrayBuffer)
+      parser = {
+        dataView: dataView,
+        position: 2
+      }
+      result = index.__get__("parseUint8")(parser)
+    })
+    it("returns the correct number", () => expect(result).toEqual(240))
+    it("does not modify the array buffer", () => expect(Array.from(new Uint8Array(arrayBuffer))).toEqual([77, 27, 240, 195, 116]))
+    it("does not modify the data view", () => expect(parser.dataView).toBe(dataView))
+    it("increments the position to clear the read data", () => expect(parser.position).toEqual(3))
+  })
+
+  describe("parseUint8Array", () => {
+    let arrayBuffer, dataView, parser, result
+    beforeEach(() => {
+      arrayBuffer = new Uint8Array([77, 27, 240, 195, 116, 65, 8, 21, 77, 250]).buffer
+      dataView = new DataView(arrayBuffer)
+      parser = {
+        dataView: dataView,
+        position: 2
+      }
+      result = index.__get__("parseUint8Array")(parser, 3)
+    })
+    it("returns a Uint8Array", () => expect(result).toEqual(jasmine.any(Uint8Array)))
+    it("returns the correct quantity of numbers", () => expect(result.length).toEqual(3))
+    it("returns the correct number", () => {
+      expect(result[0]).toEqual(240)
+      expect(result[1]).toEqual(195)
+      expect(result[2]).toEqual(116)
+    })
+    it("does not modify the array buffer", () => expect(Array.from(new Uint8Array(arrayBuffer))).toEqual([77, 27, 240, 195, 116, 65, 8, 21, 77, 250]))
+    it("does not modify the data view", () => expect(parser.dataView).toBe(dataView))
+    it("increments the position to clear the read data", () => expect(parser.position).toEqual(5))
+  })
+
+  describe("parseUint16", () => {
+    let arrayBuffer, dataView, parser, result
+    beforeEach(() => {
+      arrayBuffer = new Uint8Array([77, 27, 240, 195, 116, 111, 8]).buffer
+      dataView = new DataView(arrayBuffer)
+      parser = {
+        dataView: dataView,
+        position: 2
+      }
+      result = index.__get__("parseUint16")(parser)
+    })
+    it("returns the correct number", () => expect(result).toEqual(50160))
+    it("does not modify the array buffer", () => expect(Array.from(new Uint8Array(arrayBuffer))).toEqual([77, 27, 240, 195, 116, 111, 8]))
+    it("does not modify the data view", () => expect(parser.dataView).toBe(dataView))
+    it("increments the position to clear the read data", () => expect(parser.position).toEqual(4))
+  })
+
+  describe("parseUint16Array", () => {
+    let arrayBuffer, dataView, parser, result
+    beforeEach(() => {
+      arrayBuffer = new Uint8Array([77, 27, 240, 195, 116, 65, 8, 21, 77, 250]).buffer
+      dataView = new DataView(arrayBuffer)
+      parser = {
+        dataView: dataView,
+        position: 2
+      }
+      result = index.__get__("parseUint16Array")(parser, 3)
+    })
+    it("returns a Uint16Array", () => expect(result).toEqual(jasmine.any(Uint16Array)))
+    it("returns the correct quantity of numbers", () => expect(result.length).toEqual(3))
+    it("returns the correct number", () => {
+      expect(result[0]).toEqual(50160)
+      expect(result[1]).toEqual(16756)
+      expect(result[2]).toEqual(5384)
+    })
+    it("does not modify the array buffer", () => expect(Array.from(new Uint8Array(arrayBuffer))).toEqual([77, 27, 240, 195, 116, 65, 8, 21, 77, 250]))
+    it("does not modify the data view", () => expect(parser.dataView).toBe(dataView))
+    it("increments the position to clear the read data", () => expect(parser.position).toEqual(8))
+  })
+
+  describe("parseFloat32", () => {
+    let arrayBuffer, dataView, parser, result
+    beforeEach(() => {
+      arrayBuffer = new Uint8Array([77, 27, 240, 195, 116, 65, 8, 21, 77, 250]).buffer
+      dataView = new DataView(arrayBuffer)
+      parser = {
+        dataView: dataView,
+        position: 2
+      }
+      result = index.__get__("parseFloat32")(parser)
+    })
+    it("returns the correct number", () => expect(result).toBeCloseTo(15.2978363037109))
+    it("does not modify the array buffer", () => expect(Array.from(new Uint8Array(arrayBuffer))).toEqual([77, 27, 240, 195, 116, 65, 8, 21, 77, 250]))
+    it("does not modify the data view", () => expect(parser.dataView).toBe(dataView))
+    it("increments the position to clear the read data", () => expect(parser.position).toEqual(6))
+  })
+
+  describe("parseFloat32Array", () => {
+    let arrayBuffer, dataView, parser, result
+    beforeEach(() => {
+      arrayBuffer = new Uint8Array([77, 27, 240, 195, 116, 65, 96, 213, 117, 64, 111, 8, 21, 66, 250, 201, 102, 70, 9, 211, 53, 41, 77, 191, 67, 77, 212, 23, 30, 211, 203, 119, 10, 48, 68, 173, 151, 147, 20, 144, 97]).buffer
+      dataView = new DataView(arrayBuffer)
+      parser = {
+        dataView: dataView,
+        position: 2
+      }
+      result = index.__get__("parseFloat32Array")(parser, 3)
+    })
+    it("returns a Float32Array", () => expect(result).toEqual(jasmine.any(Float32Array)))
+    it("returns the correct quantity of numbers", () => expect(result.length).toEqual(3))
+    it("returns the correct number", () => {
+      expect(result[0]).toBeCloseTo(15.2978363037109)
+      expect(result[1]).toBeCloseTo(3.84114837646484)
+      expect(result[2]).toBeCloseTo(37.2582359313965)
+    })
+    it("does not modify the array buffer", () => expect(Array.from(new Uint8Array(arrayBuffer))).toEqual([77, 27, 240, 195, 116, 65, 96, 213, 117, 64, 111, 8, 21, 66, 250, 201, 102, 70, 9, 211, 53, 41, 77, 191, 67, 77, 212, 23, 30, 211, 203, 119, 10, 48, 68, 173, 151, 147, 20, 144, 97]))
+    it("does not modify the data view", () => expect(parser.dataView).toBe(dataView))
+    it("increments the position to clear the read data", () => expect(parser.position).toEqual(14))
+  })
+
+  describe("parseUtf8", () => {
+    let arrayBuffer, dataView, parser, result
+    beforeEach(() => {
+      arrayBuffer = new Uint8Array([77, 27, 0x54, 0x65, 0x73, 0x74, 0x20, 0x53, 0x74, 0x72, 0x69, 0x6e, 0x67, 0x20, 0x57, 0x69, 0x74, 0x68, 0x20, 0x53, 0x79, 0x6d, 0x62, 0x6f, 0x6c, 0x73, 0x3a, 0x20, 0xe3, 0x81, 0xb2, 0xc3, 0xa9, 0xf0, 0xa1, 0x81, 0xaf, 0x21, 0, 147, 20, 144, 97]).buffer
+      dataView = new DataView(arrayBuffer)
+      parser = {
+        dataView: dataView,
+        position: 2
+      }
+      result = index.__get__("parseUtf8")(parser)
+    })
+    it("returns the correct string", () => expect(result).toEqual("Test String With Symbols: ひé𡁯!"))
+    it("does not modify the array buffer", () => expect(Array.from(new Uint8Array(arrayBuffer))).toEqual([77, 27, 0x54, 0x65, 0x73, 0x74, 0x20, 0x53, 0x74, 0x72, 0x69, 0x6e, 0x67, 0x20, 0x57, 0x69, 0x74, 0x68, 0x20, 0x53, 0x79, 0x6d, 0x62, 0x6f, 0x6c, 0x73, 0x3a, 0x20, 0xe3, 0x81, 0xb2, 0xc3, 0xa9, 0xf0, 0xa1, 0x81, 0xaf, 0x21, 0, 147, 20, 144, 97]))
+    it("does not modify the data view", () => expect(parser.dataView).toBe(dataView))
+    it("increments the position to clear the read data", () => expect(parser.position).toEqual(39))
+  })
 })
--- src/file.js
+++ src/file.js
@@ -6,3 +6,71 @@ function readFileAsArrayBuffer(url, then) {
   request.onload = event => then(request.response)
   request.send()
 }
+
+function createParser(arrayBuffer) {
+  return {
+    dataView: new DataView(arrayBuffer),
+    position: 0
+  }
+}
+
+function parseUint8(parser) {
+  const output = parser.dataView.getUint8(parser.position)
+  parser.position++
+  return output
+}
+
+function parseUint8Array(parser, count) {
+  const output = new Uint8Array(count)
+  for (let i = 0; i < count; i++) output[i] = parseUint8(parser)
+  return output
+}
+
+function parseUint16(parser) {
+  const output = parser.dataView.getUint16(parser.position, true)
+  parser.position += 2
+  return output
+}
+
+function parseUint16Array(parser, count) {
+  const output = new Uint16Array(count)
+  for (let i = 0; i < count; i++) output[i] = parseUint16(parser)
+  return output
+}
+
+function parseFloat32(parser) {
+  const output = parser.dataView.getFloat32(parser.position, true)
+  parser.position += 4
+  return output
+}
+
+function parseFloat32Array(parser, count) {
+  const output = new Float32Array(count)
+  for (let i = 0; i < count; i++) output[i] = parseFloat32(parser)
+  return output
+}
+
+function parseUtf8(parser) {
+  let output = ""
+  while (true) {
+    const firstByte = parseUint8(parser)
+    if (!firstByte) return output
+    let charCode = firstByte
+
+    if ((firstByte & 224) == 192) {
+      charCode = (charCode & 31) * 64
+      charCode += parseUint8(parser) & 63
+    } else if ((firstByte & 240) == 224) {
+      charCode = (charCode & 15) * 4096
+      charCode += (parseUint8(parser) & 63) * 64
+      charCode += parseUint8(parser) & 63
+    } else if ((firstByte & 248) == 240) {
+      charCode = (charCode & 7) * 262144
+      charCode += (parseUint8(parser) & 63) * 4096
+      charCode += (parseUint8(parser) & 63) * 64
+      charCode += parseUint8(parser) & 63
+    }
+
+    output += String.fromCodePoint(charCode)
+  }
+}
