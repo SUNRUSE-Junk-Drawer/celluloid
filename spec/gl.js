4962787ab83182dcc5b49e4104198911713a71b3
jameswilddev <jameswilddev@outlook.com>
2018-04-07 13:46:14 +0100

    Added variables to be used by the GL subsystem.

--- spec/gl.js
+++ spec/gl.js
@@ -3,6 +3,9 @@ describe("gl", () => {
   let index
   index = rewire("../dist/index")
 
+  it("defines gl as null", () => expect(index.__get__("gl")).toBeNull())
+  it("defines glNonce as a number", () => expect(index.__get__("glNonce")).toEqual(jasmine.any(Number)))
+
   describe("handleContextLost", () => {
     beforeEach(() => index.__get__("handleContextLost")())
     it("does nothing", () => { })
--- src/gl.js
+++ src/gl.js
@@ -1,3 +1,6 @@
+let gl = null
+let glNonce = 0
+
 function handleContextLost() {
 
 }
