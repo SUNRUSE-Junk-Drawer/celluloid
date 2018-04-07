77d1d1fe41efa62027f9200200e2689c8b875c6e
jameswilddev <jameswilddev@outlook.com>
2018-04-07 12:22:25 +0100

    Fixed a reference to the nonexistent function stopRenderLoop.

--- spec/error-handling.js
+++ spec/error-handling.js
@@ -6,14 +6,19 @@ describe("errorHandling", () => {
   it("defines errorEncountered as null", () => expect(index.__get__("errorEncountered")).toBeNull())
 
   describe("handleError", () => {
-    let stopRenderLoop
+    let checkRenderLoop, errorEncounteredAtTimeOfCheckingRenderLoop
     beforeEach(() => {
-      stopRenderLoop = jasmine.createSpy("stopRenderLoop")
-      index.__set__("stopRenderLoop", stopRenderLoop)
+      checkRenderLoop = jasmine.createSpy("checkRenderLoop")
+      checkRenderLoop.and.callFake(() => errorEncounteredAtTimeOfCheckingRenderLoop = index.__get__("errorEncountered"))
+      index.__set__("checkRenderLoop", checkRenderLoop)
+      errorEncounteredAtTimeOfCheckingRenderLoop = null
       index.__get__("handleError")("Test Message", "Test Source File", 398, 289, "Test Error")
     })
-    it("stops the render loop once", () => expect(stopRenderLoop).toHaveBeenCalledTimes(1))
     it("sets errorEncountered to describe the error", () => expect(index.__get__("errorEncountered")).toEqual(`Unhandled error on line 398, column 289 of "Test Source File":
+\t"Test Message"
+\t"Test Error"`))
+    it("checks the render loop once", () => expect(checkRenderLoop).toHaveBeenCalledTimes(1))
+    it("has set errorEncountered by the time of checking the render loop", () => expect(errorEncounteredAtTimeOfCheckingRenderLoop).toEqual(`Unhandled error on line 398, column 289 of "Test Source File":
 \t"Test Message"
 \t"Test Error"`))
   })
--- src/error-handling.js
+++ src/error-handling.js
@@ -2,5 +2,5 @@ let errorEncountered = null
 
 function handleError(message, source, lineno, colno, error) {
   errorEncountered = "Unhandled error on line " + lineno + ", column " + colno + " of \"" + source + "\":\n\t\"" + message + "\"\n\t\"" + error + "\""
-  stopRenderLoop()
+  checkRenderLoop()
 }
