3c68f3483680349c3713b58d95b0482e1574e95e
jameswilddev <jameswilddev@outlook.com>
2018-12-14 07:57:47 +0000

    Removed JSHint (won't be needed once Typescript is in).

--- concat-jshint.js
+++
@@ -1,9 +0,0 @@
-import glob from "glob"
-import concat from "concat"
-
-glob("src/**.js", (err, matches) => {
-  if (err) throw err
-  matches = matches.filter(match => !/^src(\/|\\)header.js$/.test(match))
-  matches.unshift("src/header.js")
-  concat(matches, "dist/jshint.js")
-})
